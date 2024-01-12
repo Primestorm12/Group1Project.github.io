d45497dc84cea3b2dc6e2aba17422a9941730370
Primestorm12 <141392646+Primestorm12@users.noreply.github.com>
2024-01-12 15:15:10 +0800

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,40 @@
+const chartData = {
+    labels: ["HUMMS", "STEM", "TVL", "ABM",],
+    data: [30, 24, 16, 20,],
+  };
+  
+  const myChart = document.querySelector(".my-chart");
+  const ul = document.querySelector(".programming-stats .details ul");
+  
+  new Chart(myChart, {
+    type: "doughnut",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "Strand Popularity",
+          data: chartData.data,
+        },
+      ],
+    },
+    options: {
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+  
+  const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
+    });
+  };
+  
+  populateUl();
